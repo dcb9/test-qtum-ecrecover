d446fc887f26cfc066a612817033ca0a31dd0ed9
dcb9 <bob@phpor.me>
2018-09-21 16:28:51 +0800

    Print more info

--- crypto.js
+++ crypto.js
@@ -6,9 +6,10 @@ const { PrivateKey, Networks } = require('qtumcore-lib');
 // 
 // /dapp # qcli gethexaddress qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW
 // 7926223070547d2d15b2ef5e7383e541c338ffe9
-const privateKey = PrivateKey.fromWIF('cMbgxCJrTYUqgcmiC1berh5DFrtY1KeU4PXZ6NZxgenniF1mXCRk');
+const privateKey = PrivateKey.fromWIF('cRcG1jizfBzHxfwu68aMjhy78CpnzD9gJYZ5ggDbzfYD3EQfGUDZ');
 
 const privateKeyHex = '0x' + privateKey.toString().padStart(64, '0');
+console.log("private key:", privateKeyHex);
 
 let message = "hi";
 message = "\x19Ethereum Signed Message:\n" + message.length + message
@@ -16,3 +17,5 @@ const signature = web3.eth.accounts.sign(message,privateKeyHex);
 
 console.log("message hash:", signature.messageHash);
 console.log("signature:", signature.signature);
+
+console.log("addr in ETH format:", web3.eth.accounts.recover(message, signature.signature));
